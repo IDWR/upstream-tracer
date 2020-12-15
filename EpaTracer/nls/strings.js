072c7c6e067349019172f57cc29667f8705ff437
benbritton314 <66647826+benbritton314@users.noreply.github.com>
2020-12-15 15:09:07 -0700

    Changed location marker and made configuration changes.
    Changed location marker.  Rearranged some of the elements in the widget's user interface.  Added a help-file that is specific to the IDWR but can be used as an example of the capabilities of the widget.

--- EpaTracer/nls/strings.js
+++ EpaTracer/nls/strings.js
@@ -2,7 +2,7 @@
 define({
   root: ({
     _widgetLabel: "EPA Tracer", // widget label shown on the choose widget dialog and widget panel title
-    searchHeaderText: "Search for measuring location", // Shown as a label in widget panel for search an address.
+    searchHeaderText: "Click on map or search by measuring location ID", // Shown as a label in widget panel for search an address.
     invalidSearchLayerMsg: "Search layers are not configured properly", // Shown as a message when the configured search layer is invalid or no layer is configured
     bufferSliderText: "Show results within ${BufferDistance} ${BufferUnit}", // Shown as a label for slider to display the result in buffer area.
     bufferTextboxLabel: "Show results within (${BufferUnit})", // Shown as a label for slider to display the result in buffer area.
@@ -52,5 +52,42 @@ define({
         acronym: "m"
       }
     }
-  })
+  }),
+  "ar": 1,
+  "bs": 1,
+  "ca": 1,
+  "cs": 1,
+  "da": 1,
+  "de": 1,
+  "el": 1,
+  "es": 1,
+  "et": 1,
+  "fi": 1,
+  "fr": 1,
+  "he": 1,
+  "hr": 1,
+  "hu": 1,
+  "it": 1,
+  "id": 1,
+  "ja": 1,
+  "ko": 1,
+  "lt": 1,
+  "lv": 1,
+  "nb": 1,
+  "nl": 1,
+  "pl": 1,
+  "pt-br": 1,
+  "pt-pt": 1,
+  "ro": 1,
+  "ru": 1,
+  "sl": 1,
+  "sr": 1,
+  "sv": 1,
+  "th": 1,
+  "tr": 1,
+  "uk": 1,
+  "vi": 1,
+  "zh-cn": 1,
+  "zh-hk": 1,
+  "zh-tw": 1
 });
